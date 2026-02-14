5dab594f603f3506de42dbe0ae1b40a6347f3b0a
松村知生 <matsumurachisei1218@gmail.com>
2026-02-14 21:27:36 +0900

    tailwind使えるようにした。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/main/resources/templates/**/*.html",
+    "./src/main/resources/templates/**/*.{html,js}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
